bd703f830ccc881f74284d5733cfb5b0b1e8d5b6
EFG <efg@efg.local>
2025-09-09 21:15:55 -0300

    primeiro commit

---
+++ api_dados_abertos.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+def get_request(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        # print(json.dumps(response.json(), indent=2))
+        return response.json()
+    else:
+        print("Erro ao buscar dados da url")
+        return None
+
+def get_deputados():
+    deputados_url = "https://dadosabertos.camara.leg.br/api/v2/deputados"
+    data = get_request(deputados_url)
+    return data.get("dados")
+
+def get_despesas(id_deputado):
+    despesas_url = f'https://dadosabertos.camara.leg.br/api/v2/deputados/{id_deputado}/despesas'
+    data = get_request(despesas_url)
+    return data.get("dados")
---
+++ database_queries.py
@@ -0,0 +1,76 @@
+import database_connection as db_conn
+import mysql.connector
+
+def executar_query(sql, dados):
+    conn = db_conn.conectar()
+    if conn is None:
+        print("conexão vazia")
+        return
+    
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, dados)
+        conn.commit()
+        print("Comando SQL executado com sucesso!")
+    except mysql.connector.Error as error:
+        print(f"Erro ao executar comando: {error}")
+        conn.rollback()
+    finally:
+        cur.close()
+        conn.close()
+
+def consultar_query(sql, dados):
+    conn = db_conn.conectar()
+    if conn is None:
+        print("conexão vazia")
+        return
+    resultados = None
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, dados)
+        resultados = cur.fetchall()
+        print("Comando SQL executado com sucesso!")
+    except mysql.connector.Error as error:
+        print(f"Erro ao executar comando: {error}")
+        conn.rollback()
+    finally:
+        cur.close()
+        conn.close()
+    return resultados
+def criar_tabela_deputados():
+    sql = """
+    CREATE TABLE IF NOT EXISTS deputado(
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        mat_deputado INT UNIQUE NOT NULL,
+        nome VARCHAR(100) NOT NULL,
+        partido VARCHAR(100) NOT NULL,
+        uf VARCHAR(4) NOT NULL,
+        total_gastos NUMERIC(10, 2)
+    );"""
+    executar_query(sql)
+
+def inserir_deputado(deputado, total_gastos):
+    matricula = deputado['id']
+    nome = deputado['nome']
+    partido = deputado['siglaPartido']
+    uf = deputado['siglaUf']
+
+    sql = """
+        INSERT IGNORE INTO deputado (
+        mat_deputado,
+        nome,
+        partido,
+        uf,
+        total_gastos
+        ) VALUES (%s, %s, %s, %s, %s)
+    """
+    dados = (matricula, nome, partido, uf, total_gastos)
+    executar_query(sql, dados)
+    
+def get_all_gastos_by_partudo():
+    sql = """
+        SELECT partido, SUM(total_gastos) AS gastos_totais
+        FROM deputado
+        GROUP BY partido;
+    """
+    consultar_query(sql)
