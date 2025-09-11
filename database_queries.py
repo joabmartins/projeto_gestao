a18b37becce298b817364b64f99a588f114dbac9
EFG <efg@efg.local>
2025-09-10 21:23:59 -0300

    gráfico

--- database_queries.py
+++ database_queries.py
@@ -19,7 +19,7 @@ def executar_query(sql, dados):
         cur.close()
         conn.close()
 
-def consultar_query(sql, dados):
+def consultar_query(sql):
     conn = db_conn.conectar()
     if conn is None:
         print("conexão vazia")
@@ -27,7 +27,7 @@ def consultar_query(sql, dados):
     resultados = None
     try:
         cur = conn.cursor()
-        cur.execute(sql, dados)
+        cur.execute(sql)
         resultados = cur.fetchall()
         print("Comando SQL executado com sucesso!")
     except mysql.connector.Error as error:
@@ -68,10 +68,18 @@ def inserir_deputado(deputado, total_gastos):
     dados = (matricula, nome, partido, uf, total_gastos)
     executar_query(sql, dados)
 
-    def get_all_gastos_by_partido():
-        sql = """
-            SELECT partido, SUM(total_gastos) AS gastos_totais
-            FROM deputados
-            GROUP BY partido;
-        """
-        consultar_query(sql)
+def get_all_gastos_by_partido():
+    sql = """
+        SELECT partido, SUM(total_gastos) AS gastos_totais
+        FROM deputado
+        GROUP BY partido;
+    """
+    return consultar_query(sql)
+
+def get_all_gastos_by_uf():
+    sql = """
+        SELECT uf, SUM(total_gastos) as gastos_totais
+        FROM deputado
+        GROUP BY uf;
+    """
+    return consultar_query(sql)
--- service.py
+++ service.py
@@ -1,5 +1,10 @@
 import database_queries as query
 import api_dados_abertos as api
+import json
+# pip install pandas matplotlib seaborn
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 #query.criar_tabela_deputados()
 #comandos a serem executados apenas uma vez
@@ -25,4 +30,24 @@ def get_total_despesas(id):
         total_gastos += despesa.get("valorLiquido", 0)
     return total_gastos
 
-cadastrar_deputados()
+#comandos a serem executados apenas uma vez
+#cadastrar_deputados()
+
+def apresentar_dados():
+    gastos_partido = query.get_all_gastos_by_partido()
+    gastos_uf = query.get_all_gastos_by_uf()
+    # converter JSON em dataframe do Pandas
+    df = pd.DataFrame(gastos_partido, columns=['partido', 'gastos_totais'])
+    df_uf = pd.DataFrame(gastos_uf, columns=['uf', 'gastos_totais'])
+    # criar o gráfico de barras
+    #plt.figure(figsize=(10, 6))
+    fig, (partido_gf, uf_gf) = plt.subplots(1, 2, figsize=(18, 6))
+    partido_gf = sns.barplot(x='partido', y='gastos_totais', data=df, ax=partido_gf)
+    uf_gt = sns.barplot(x='uf', y='gastos_totais', data=df_uf, ax=uf_gf)
+    partido_gf.tick_params(axis='x', rotation=45)
+    uf_gf.tick_params(axis='x', rotation=45)
+    # exibir o gráfico
+    plt.tight_layout()
+    plt.show()
+    
+apresentar_dados()
